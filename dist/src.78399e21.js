3ba9fa8a36cdeb75c18fd272354a465d4d7c5854
Ty4jbt <tyaboe@gmail.com>
2021-06-09 11:02:05 -0700

    new merged main

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -51894,11 +51894,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52929" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61263" + '/');
->>>>>>> testtest
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61274" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
