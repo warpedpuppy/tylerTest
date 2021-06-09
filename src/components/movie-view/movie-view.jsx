f4b00b0551d439bc460e738870ac4fe0f165067d
Ty4jbt <tyaboe@gmail.com>
2021-06-09 02:05:41 -0700

    added more buttons for functionality

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -31,7 +33,15 @@ export class MovieView extends React.Component {
                     <span className='label'>Description: </span>
                     <span className='value'>{movie.Description}</span>
                 </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
+                <Link to={`/directors/${movie.Director.Name}`}>
+                    <Button variant='link'>Director</Button>
+                </Link>
+                <Link to={`/genres/${movie.Genre.Name}`}>
+                    <Button variant='link'>Genre</Button>
+                </Link>
+                <Link to={`/`}>
+                    <Button variant='primary'>Back</Button>
+                </Link>
             </div>
         );
     }
