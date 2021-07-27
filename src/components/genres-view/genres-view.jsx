0ded885f8d4e7d1ce292cec2eec629d0a807dc4d
Ty4jbt <tyaboe@gmail.com>
2021-07-27 15:43:18 -0700

    connected and added features to genre-view

--- src/components/genres-view/genres-view.jsx
+++ src/components/genres-view/genres-view.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 export function GenresView(props) {
     console.log(props);
     let genresName = props.movies[0].Genre.Name
     return(
         <div>
+            <Link to={`/`}>
+                <Button variant='link'>Home</Button>
+            </Link>
             <h1>{genresName}</h1>
             {props.movies.map((movie) => (
                 <Col md={3} key={movie._id}>
-                    <MovieCard movie={movie} />
+                    <MovieCard
+                        user={props.user}
+                        userData={props.userData}
+                        addFavoriteToUserData={props.addFavoriteToUserData}
+                        removeFavoriteFromUserData={props.removeFavoriteFromUserData} 
+                        movie={movie}
+                    />
                 </Col>
             ))}
         </div>
